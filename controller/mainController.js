2bfb1bb8ab79bd8007a7d80b4a4f36e175e5ac3a
Yzoraa <yezing01@naver.com>
2025-03-11 18:40:37 +0900

    Feat: 상세페이지 연동

--- controller/mainController.js
+++ controller/mainController.js
@@ -5,6 +5,7 @@ const userModel = db.users; // users 모델 사용
 const categoryModel = db.category; // category 모델 사용
 const moment = require("moment");
 
+// 필요한 데이터 요청
 const getMainBoard = async (req, res) =>{
     const {categoryId} = req.query;
 
@@ -45,7 +46,7 @@ const getMainBoard = async (req, res) =>{
             img_url: x.img_url,
             userCheck: userId !== null ? userId === x.userId : false, // 작성한 유저와 동일한지 체크
         }));
-        console.log("나타날 리스트 확인: ", boardData);
+        // console.log("나타날 리스트 확인: ", boardData);
 
         res.json( {results: true , data: boardData }); // for 메인페이지 리스트 출력
         // render("") // for 상세페이지 이동? 정보전달?
@@ -53,6 +54,49 @@ const getMainBoard = async (req, res) =>{
     } catch (error) {
         console.error("메인 리스트 불러오기 오류:", error);
       }
+};
+
+// 상세페이지 이동
+const getDetailBoard = async (req, res) =>{
+    const {id} = req.params;
+
+    let userId = null; // 기본값을 null로 설정
+    if (req.user) {
+        userId = req.user.id;
+    }
+    
+    try{
+        const board = await boardModel.findOne({
+            where: {id},
+            include: [
+                { model: userModel, as: "author", attributes: ["nickname"] },
+                { model: categoryModel, as: "category", attributes: ["name"] },
+            ]
+        });
+
+        if (!board) {
+            return res.json({ result: false, message: "게시글을 찾을 수 없습니다." });
+        };
+
+        const boardData = {
+            id: board.id, // 게시판 PK
+            categoryName: board.category.name, // category의 이름
+            title: board.title,
+            nickname: board.author.nickname, // users의 별명
+            content: board.content,
+            updatedAt: moment(board.updatedAt).format("YYYY-MM-DD HH:mm"),
+            likeCount: board.likeCount,
+            img_url: board.img_url,
+            userCheck: userId !== null ? userId === board.userId : false, // 작성한 유저와 동일한지 체크
+        };
+        // console.log("해당 상세페이지 확인: ", boardData);
+
+        // res.json({ result: true, data: boardData });
+        res.render("detailpage", { data: boardData });
+
+    } catch (error) {
+        console.error("상세페이지 오류:", error);
+    }
 }
 
 // 제목 기준 검색
@@ -71,6 +115,6 @@ const searchTitle = async (req, res) =>{
     } catch (error) {
         console.error("검색 체크 오류:", error);
     }
-}
+};
 
-module.exports = { getMainBoard, searchTitle };
+module.exports = { getMainBoard, getDetailBoard, searchTitle };
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -6,6 +6,9 @@ const optionMiddleware = require("../middleware/optionMiddleware");
 // 메인 게시글 불러오기
 router.get("/boardData", optionMiddleware, mainController.getMainBoard);
 
+// 게시글 상세 조회
+router.get("/move/detail/:id", mainController.getDetailBoard);
+
 // 검색기능 (제목기준)
 router.get("/search", mainController.searchTitle);
 
